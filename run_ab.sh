fde2d10b4eb46d7912a7fac3af09119ac7f0fd45
mozhuli <21621232@zju.edu.cn>
2017-12-29 17:45:21 +0800

    Add e2e-ab

--- run_ab.sh
+++ run_ab.sh
@@ -9,4 +9,10 @@ RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 sleep 10
 
 /usr/bin/ab -n 90000 -c 50 ${NGINX_ENDPOINT} >"${RESULTS_DIR}/ab"
-echo -n "${RESULTS_DIR}/ab" >"${RESULTS_DIR}/done"
+
+echo "ab done"
+
+cd ${RESULTS_DIR}
+tar -czf e2e-ab.tar.gz *
+# mark the done file as a termination notice.
+echo -n ${RESULTS_DIR}/e2e-ab.tar.gz > ${RESULTS_DIR}/done
